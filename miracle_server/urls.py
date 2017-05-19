922d40e5c27ad157101948cd2f975b4609f1da7a
yudong <664724224@qq.com>
2017-05-19 11:54:19 +0800

    fixed home url pattern with suffix $

--- miracle_server/urls.py
+++ miracle_server/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^flowers/', include('flowers.urls', namespace='flowers')),
     url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
-    url(r'^/', views.home)
+    url(r'^/$', views.home)
 ]
