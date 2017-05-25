5f13e41b39721f88de7d267ce8318da6bd5468ce
yudong <664724224@qq.com>
2017-05-25 16:17:09 +0800

    add extra fields to flower model

--- flowers/models.py
+++ flowers/models.py
@@ -16,6 +16,9 @@ class Flower(models.Model):
     seasons = models.CharField(max_length=100, blank=True, choices=SEASONS, null=True)
     place = models.CharField(max_length=100, blank=True, null=True)
     image = models.ImageField(upload_to='uploads/flower', blank=True, null=True)
+    smell = models.CharField(max_length=100, blank=True, null=True)
+    scenarios = models.CharField(max_length=100, blank=True, null=True)
+    slogan = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return self.title
