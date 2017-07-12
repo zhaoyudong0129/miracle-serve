52f5bd9668a46f439620592a6063d403f01476c3
yudong <664724224@qq.com>
2017-07-12 18:02:04 +0800

    add restart miracle to build script

--- dev/pipline.sh
+++ dev/pipline.sh
@@ -7,3 +7,5 @@ echo "***start migrate ***"
 python manage.py migrate
 echo "***start collectstatic***"
 python manage.py collectstatic
+echo "***restart miracle***"
+supervisorctl -c dev/supervisord.conf restart miracle
