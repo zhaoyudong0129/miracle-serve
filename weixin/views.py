cc14d95b4a483ed4f75c6f52ba6db0b20e726341
yudong <664724224@qq.com>
2017-07-13 20:04:47 +0800

    add wechat-sdk plugin

--- weixin/views.py
+++ weixin/views.py
@@ -8,30 +8,26 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
+from wechat_sdk import WechatBasic
 
 logger = logging.getLogger(__name__)
 
 
 @api_view(['GET', 'POST'])
 def home(request):
-    signature = request.query_params.get('signature','none')
-    timestamp = request.query_params.get('timestamp','none')
-    nonce = request.query_params.get('nonce','none')
-    echostr = request.query_params.get('echostr','none')
+    signature = request.query_params.get('signature', 'none')
+    timestamp = request.query_params.get('timestamp', 'none')
+    nonce = request.query_params.get('nonce', 'none')
+    echostr = request.query_params.get('echostr', 'none')
 
     logger.info('signature:{},timestamp:{},nonce:{},echostr:{}'.format(signature, timestamp, nonce, echostr))
 
     token = 'hello123'
 
-    weixin_list = [token, timestamp, nonce]
-    weixin_list.sort()
-    sha1 = hashlib.sha1()
-
-    map(sha1.update, weixin_list)
-    hashcode = sha1.hexdigest()
+    wechat = WechatBasic(token=token)
 
     if request.method == 'GET':
-        if hashcode == signature:
+        if wechat.check_signature(signature, timestamp, nonce):
             logger.info('weixin token check success')
             return HttpResponse(echostr)
         else:
